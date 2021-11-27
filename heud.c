cb873d5612e9d2a4427368f638bfa0acdbc34119
Giuseppe Coviello <gcov@pm.me>
2021-11-27 01:37:34 +0100

    Marked local functions as static

--- heud.c
+++ heud.c
@@ -11,22 +11,14 @@ struct libevdev *kbd_dev;
 int uinput_fd;
 struct libevdev_uinput *virtkbd_dev;
 
-void init();
-void finalize();
-void grab_keyboard(const char *dev);
-void create_virtual_keyboard();
-void emit_event(struct input_event *event);
-void capture_key_event(struct input_event *events);
-int capture_event(struct input_event *event, unsigned type, int retry);
-unsigned int replace_key(unsigned int code, unsigned int *modifier);
+static void init();
+static void cleanup();
+static void grab_keyboard(const char *dev);
+static void create_virtual_keyboard();
+static void emit_event(struct input_event *event);
+static int capture_event(struct input_event *event, unsigned type, int retry);
+static unsigned int replace_key(unsigned int code, unsigned int *modifier);
 
-const char *event_type(unsigned short type);
-
-
-void emit(int fd, struct input_event ie) {
-	write(fd, &ie, sizeof(ie));
-	fsync(fd);
-}
 
 int main(int argc, char **argv) {
 	if (argc != 2) {
@@ -38,8 +30,6 @@ int main(int argc, char **argv) {
 	create_virtual_keyboard();
 
 	int leftalt_pressed = 0;
-	unsigned int code;
-	int value;
 
 	struct input_event event;
 	unsigned int replacement, modifier;
@@ -48,14 +38,12 @@ int main(int argc, char **argv) {
 	
 	for (;;) {
 		capture_event(&event, EV_KEY, 1);
-
 		is_press = event.value == 1;
 		is_release = event.value == 0;
 
 		if (!is_press && !is_release) {
 			continue;
 		}
-		code = event.code;
 		
 		if (event.code == KEY_LEFTALT) {
 			leftalt_pressed = is_press; 
@@ -82,20 +70,22 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
-void init() {
+static void init() {
 	kbd_fd = 1;
 	uinput_fd = -1;
-	atexit(finalize);
+	atexit(cleanup);
 }
 
-void finalize() {
-	printf("Finalizing\n");
-
+static void cleanup() {
 	ioctl(kbd_fd, EVIOCGRAB, NULL);
 	close(kbd_fd);
+
+	libevdev_uinput_destroy(virtkbd_dev);
+	libevdev_free(kbd_dev);
+	close(uinput_fd);
 }
 
-void grab_keyboard(const char *dev) {
+static void grab_keyboard(const char *dev) {
 	kbd_fd = open(dev, O_RDWR);
 	if (kbd_fd < 0) {
 		fprintf(stderr, "Error opening keyboard at '%s'\n", dev);
@@ -114,7 +104,7 @@ void grab_keyboard(const char *dev) {
 	}
 }
 
-void create_virtual_keyboard() {
+static void create_virtual_keyboard() {
 	uinput_fd = open("/dev/uinput", O_RDWR);
 	if (uinput_fd < 0) {
 		fprintf(stderr, "Error opening /dev/uinput\n");
@@ -127,11 +117,7 @@ void create_virtual_keyboard() {
 	}
 }
 
-void emit_event(struct input_event *event) {
-	printf("Emitting type: 0x%x code: %d value: %d\n",
-	       event->type, event->code, event->value);
-
-
+static void emit_event(struct input_event *event) {
 	libevdev_uinput_write_event(virtkbd_dev,
 				    event->type,
 				    event->code,
@@ -142,44 +128,19 @@ void emit_event(struct input_event *event) {
 				    0);
 }
 
-void emit_events(struct input_event *event, int len) {
-	int i;
-	for (i = 0; i < len; i++) {
-		emit_event(event + i);
-	}
-}
-
-int capture_event(struct input_event *event, unsigned type, int retry) {
+static int capture_event(struct input_event *event, unsigned type, int retry) {
 	for (;;) {
 		read(kbd_fd, event, sizeof(struct input_event));
 		if (event->type == type) {
 			return 0;
 		}
-		//printf("Expected 0x%x received 0x%x\n", type, event->type);
-		//emit_event(event);
 		if (!retry) {
 			return -1;
 		}
 	}
 }
 
-
-void capture_key_event(struct input_event *events) {
-	for (;;) {
-		capture_event(events, EV_MSC, 1);
-		if (capture_event(events + 1, EV_KEY, 0) != 0) {
-			emit_event(events);
-			continue;
-		}
-		if (capture_event(events + 2, EV_SYN, 0) != 0) {
-			emit_events(events, 2);
-			continue;
-		}
-		break;
-	}
-}
-
-unsigned int replace_key(unsigned int code, unsigned int *modifier) {
+static unsigned int replace_key(unsigned int code, unsigned int *modifier) {
 	*modifier = 0;
 	switch(code) {
 	case KEY_LEFT:
